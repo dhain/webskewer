f230c1bb8255284a9a99b2f05fa516a6a26a6dab
David Hain <dhain@zognot.org>
2009-02-03 17:55:34 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(
+    name='webskewer',
+    version=version,
+    description=('An HTTP/1.1 compliant WSGI server that '
+                 'uses a greenlet-based scheduling model'),
+    author='David Hain',
+    author_email='dhain@webskewer.org',
+    url='http://webskewer.org/',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    namespace_packages=['webskewer'],
+    install_requires=[
+        'greennet',
+        'pytz',
+    ],
+)
