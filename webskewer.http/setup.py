cf20abd8e70cedf3c36371032ff74bb94a6248da
David Hain <dhain@zognot.org>
2009-02-04 20:29:20 -0800

    make room for other namespaced packages

--- webskewer.http/setup.py
+++ webskewer.http/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 version = '0.1.0'
 
 setup(
-    name='webskewer',
+    name='webskewer.http',
     version=version,
     description=('An HTTP/1.1 compliant WSGI server that '
                  'uses a greenlet-based scheduling model'),
